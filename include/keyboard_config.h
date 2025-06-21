e9db577d3c4e1a928b6bc28165aec51f08385b17
ohmdelta <64962148+ohmdelta@users.noreply.github.com>
2025-06-21 22:00:38 +0100

    add debounce code

---
+++ include/keyboard_config.h
@@ -0,0 +1,51 @@
+#ifndef KEYBOARD_CONFIG_H
+#define KEYBOARD_CONFIG_H
+
+#define A1 4
+#define A2 5
+#define A3 6
+#define A4 7
+#define A5 8
+
+#define B1 9
+#define B2 10
+#define B3 11
+#define B4 12
+#define B5 13
+
+#define C1 14
+#define C2 15
+#define C3 16
+#define C4 17
+#define C5 18
+
+#define D1 19
+#define D2 20
+#define D3 21
+#define D4 22
+#define D5 23
+
+#define E1 24
+#define E2 25
+#define E3 26
+#define E4 27
+#define E5 28
+
+#define F1 29
+#define F2 30
+#define F3 31
+#define F4 32
+#define F5 33
+
+// THUMB CLUSTER
+#define T1 34
+#define T2 35
+#define T3 36
+#define T4 37
+#define T5 38
+#define T6 39
+
+#define MATRIX_COLS 6
+#define MATRIX_ROWS 5
+
+#endif
---
+++ include/structs.h
@@ -0,0 +1,10 @@
+#ifndef STRUCTS_H
+#define STRUCTS_H
+
+#include <stdint.h>
+#include <stdlib.h>
+
+typedef uint32_t matrix_row_t;
+typedef uint32_t fast_timer_t;
+
+#endif // !DEBUG STRUCTS_H
