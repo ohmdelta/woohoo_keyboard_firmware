43c369e24b60372e64eb43d7d48c5ecee0ed0e17
ohmdelta <64962148+ohmdelta@users.noreply.github.com>
2025-08-06 01:26:03 +0100

    add git macros; add dynamic repeat for macro

--- src/keycode_buffer.c
+++ src/keycode_buffer.c
@@ -4,31 +4,47 @@
 
 void reset_keycode_buffer(keycode_buffer_t *keycode_buffer)
 {
-    memset(keycode_buffer, 0, sizeof(keycode_buffer_t));
+  memset(keycode_buffer, 0, sizeof(keycode_buffer_t));
 }
 
 bool add_keycode(keycode_buffer_t *keycode_buffer, uint8_t keycode)
 {
-    if (keycode_buffer->size < KEYCODE_BUFFER_LENGTH)
-    {
-        keycode_buffer->keycodes[keycode_buffer->size++] = keycode;
-        return true;
-    }
-    return false;
+  if (keycode_buffer->size < KEYCODE_BUFFER_LENGTH)
+  {
+    keycode_buffer->keycodes[keycode_buffer->size++] = keycode;
+    return true;
+  }
+  return false;
 }
 
-bool add_keycodes(keycode_buffer_t *keycode_buffer, const uint8_t * keycode)
+bool add_keycodes(keycode_buffer_t *keycode_buffer, const uint8_t *keycode)
 {
-    while ((keycode_buffer->size < KEYCODE_BUFFER_LENGTH) && (*keycode))
+  while ((keycode_buffer->size < KEYCODE_BUFFER_LENGTH) && (*keycode))
+  {
+    bool repeated = false;
+    uint8_t phrase_start = (keycode_buffer->size / 6) * 6;
+    for (uint8_t s = phrase_start; s < keycode_buffer->size; s++)
     {
-        keycode_buffer->keycodes[keycode_buffer->size++] = *(keycode++);
+      if (keycode_buffer->keycodes[s] == *keycode) {
+        repeated = true;
+        break;
+      }
     }
-    return true;
+
+    if (repeated) {
+      memset(keycode_buffer->keycodes + keycode_buffer->size, 0, phrase_start + 6 - keycode_buffer->size);
+      keycode_buffer->size = phrase_start + 6;
+    }
+    else {
+      keycode_buffer->keycodes[keycode_buffer->size++] = *(keycode++);
+    }
+  }
+  return true;
 }
 
 bool valid(keycode_buffer_t *keycode_buffer)
 {
-    return (keycode_buffer->size <= KEYCODE_BUFFER_LENGTH) && (keycode_buffer->completed <= keycode_buffer->size);
+  return (keycode_buffer->size <= KEYCODE_BUFFER_LENGTH) && (keycode_buffer->completed <= keycode_buffer->size);
 }
 
 void update_modifier(modifier_t *modifier, uint8_t key)
